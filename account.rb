9571af42db859892cfac8ad15184acf7343e00fa
karaAJC <KaraAJC@gmail.com>
2017-10-28 11:10:34 -0500

    implements account methods

--- account.rb
+++ account.rb
@@ -2,7 +2,17 @@
 class Account
   attr_accessor :balance, :owner
   def initialize(acct_options = {})
-    owner = User.new(acct_options[:owner])
-    balance = 100
+    @owner = User.new({ name: acct_options[:owner] })
+    @balance = 100
+  end
+
+  def deposit(amount)
+    @balance += amount
+    # goodbye method
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+    # goodbye method
   end
 end
--- user.rb
+++ user.rb
@@ -2,6 +2,6 @@
 class User
   attr_accessor :name
   def initialize(user_options = {})
-    name = user_options[:name]
+    @name = user_options[:name]
   end
 end

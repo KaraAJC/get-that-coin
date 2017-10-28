4260846cff60b411d09cd88e3ce9726f8f829f53
karaAJC <KaraAJC@gmail.com>
2017-10-28 15:34:37 -0500

    creates bank class

---
+++ bank.rb
@@ -0,0 +1,32 @@
+# SauceBank
+class Bank
+  attr_accessor :accounts
+  def initialize
+    @accounts ||= load_accounts
+  end
+
+  def load_accounts
+    []
+  end
+
+  def find_account(name)
+    @accounts.select do |acct|
+      acct.owner.name.downcase == name.downcase
+    end
+  end
+
+  def has_account?(name)
+    query = find_account(name)
+    !query.empty?
+  end
+
+  def add_account(acct)
+    puts 'account added.'
+    @accounts << acct
+  end
+
+  def remove_acct(acct)
+    find_account(acct.owner.name).delete
+    puts 'account removed.'
+  end
+end
